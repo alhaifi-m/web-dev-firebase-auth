d53992850d14e9535fe8da92443df76848821aaa
Mohammed Al-Haifi <mohammed.al-haifi@sait.ca>
2025-07-29 16:57:22 -0600

    final code full

--- app/page.js
+++ app/page.js
@@ -1,7 +1,41 @@
+'use client'
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth } from "./firebase/config"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+
+
 
 export default function Home() {
+  const [ user ] = useAuthState(auth)
+  const router = useRouter()
+  const [userSession, setUserSession] = useState(false)
+  const [isSessionLoaded, setIsSessionLoaded] = useState(false)
+
+  useEffect(()=>{
+    if(typeof window !== "undefined"){
+      const session = sessionStorage.getItem("user")
+      setUserSession(session)
+      setIsSessionLoaded(true)
+    }
+  },[])
+
+  useEffect(()=>{
+    if(isSessionLoaded && !user && !userSession){
+      router.push("/sign-in")
+    }
+  },[isSessionLoaded, user, userSession, router])
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow flex items-center justify-center p-24">
+        <h1 className="text-4xl font-bold text-white">Welcome to My App</h1>
+        {user && (
+          <p className="text-lg text-gray-900 mt-4">
+            Hello, {user.email}!
+          </p>
+        )}
+      </main>
 
     </div>
   )
